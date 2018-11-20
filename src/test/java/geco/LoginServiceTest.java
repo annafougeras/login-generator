983524ca36282d36ca38bba85fcdc086e2a096b9
vincentfougeras <fougeras.vincent@gmail.com>
2018-11-20 10:16:51 +0100

    Created LoginServiceTest class structure

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,29 @@
+package geco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginServiceTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testLoginExists() {
+    }
+
+    @Test
+    public void testAddLogin() {
+    }
+
+    @Test
+    public void testFindAllLoginsStartingWith() {
+    }
+
+    @Test
+    public void testFindAllLogins() {
+    }
+}
