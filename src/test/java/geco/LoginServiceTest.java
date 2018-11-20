cc9b9e7494220fc2a3d2b9c75bd7152b7dd71ce6
vincentfougeras <fougeras.vincent@gmail.com>
2018-11-20 10:37:22 +0100

    Covered 100% of LoginService code

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -3,27 +3,41 @@ package geco;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class LoginServiceTest {
 
+    private LoginService loginService;
+
     @Before
     public void setUp() {
+        String[] logins = { "aaa", "bbb", "ccc", "ccd" };
+        loginService = new LoginService(logins);
     }
 
     @Test
     public void testLoginExists() {
+        assertTrue(loginService.loginExists("aaa"));
     }
 
     @Test
     public void testAddLogin() {
+        loginService.addLogin("ddd");
+        assertTrue(loginService.loginExists("ddd"));
     }
 
     @Test
     public void testFindAllLoginsStartingWith() {
+        assertEquals(Arrays.asList("ccc", "ccd"), loginService.findAllLoginsStartingWith("cc"));
     }
 
     @Test
     public void testFindAllLogins() {
+        assertEquals(Arrays.asList("aaa", "bbb", "ccc", "ccd"),
+                     loginService.findAllLogins());
     }
 }
